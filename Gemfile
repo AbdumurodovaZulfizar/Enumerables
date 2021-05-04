8ead54c3914baf40ea7de3eed55632e5740ad350
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-05-05 03:19:53 +0500

    Create my_select method

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 
 gem 'guard-rspec', '~> 4.7', '>= 4.7.3', require: false
 gem 'pry', '~> 0.13.1'
-gem 'rubocop', require: false
 gem 'rspec', '~> 3.10'
+gem 'rubocop', require: false
--- main.rb
+++ main.rb
@@ -1,32 +1,35 @@
 module Enumerable
-  # my_each which is equal to .each
+  # 1.my_each
   def my_each
     return enum_for unless block_given?
-
-    arr = arr.to_a
-    i = 0
+    arr = *self
+    i=0
     while i < arr.length
       yield (arr[i])
-      i += 1
-    end
-    arr[i]
-  end
-end
-
-  #   # my_each_with_index which is equal to .each_with_index
-  def my_each_with_index
-    return enum_for unless block_given?
-
-    arr = arr.to_a
-    arr.length.times do |i|
-      yield(arr[i], i)
+      i+=1
     end
     self
   end
-end
+  # 2.my_each_with_index
+  def my_each_with_index
+    return enum_for unless block_given?
+    arr = *self
+    i=0
+    while i < arr.length
+      yield(arr[i], i)
+      i+=1
+    end
+    self
+  end
 
-
-
-
+  # 3.my_select
+  def my_select
+    return enum_for unless block_given?
+    selected_arr = []
+    my_each do |ele|
+      selected_arr << ele if yield(ele)
+    end
+    selected_arr
+  end
 
 end
